65fd6301af3daaadda817a4ce4f6167b98455b6b
julien <juliencauwet@yahoo.fr>
2018-02-22 17:48:55 +0100

    Récupération des données topos dans la page HTML sous forme de liste

--- src/main/java/com/julien/climbers/topos/TopoService.java
+++ src/main/java/com/julien/climbers/topos/TopoService.java
@@ -13,22 +13,19 @@ public class TopoService {
     @Autowired
     private static TopoRepository topoRepository;
 
-    private static List<Topo> topos = new ArrayList<>(Arrays.asList(
-                new Topo( "1","Verlaine","Le grand froid du Nord","nord"),
-                new Topo( "2","Rimbaud","La côte Atlantique","ouest"),
-                new Topo( "3","Hugo","Le Massif Central","centre"),
-                new Topo( "4","Zola","la chaleur du sud","sud"),
-                new Topo( "5","Maupassant","Les montagnes de l'est","est")
-
-        ));
+   private List<Topo> topos = new ArrayList<>(Arrays.asList(
+               new Topo( "1","Verlaine","Le grand froid du Nord","nord"),
+               new Topo( "2","Rimbaud","La côte Atlantique","ouest"),
+               new Topo( "3","Hugo","Le Massif Central","centre"),
+               new Topo( "4","Zola","la chaleur du sud","sud"),
+               new Topo( "5","Maupassant","Les montagnes de l'est","est")
+                       ));
 
     public List<Topo> getTopos(){
-        System.out.println("entrée dans gettopo1");
-       List<Topo> topos = new ArrayList<>();
-        System.out.println("compte: " + topoRepository.count());
-       topoRepository.findAll()
-                .forEach(topos::add);
-        System.out.println("entrée dans gettopo2");
+     //   System.out.println("entrée dans gettopo1");
+     //   List<Topo> topos = new ArrayList<>();
+     //   topoRepository.findAll().forEach(topos::add);
+     //   System.out.println("entrée dans gettopo2");
         return topos;
     }
 
